bfd366e33602f4147f91e556a04eef1d65163195
karlos23234 <israyelvardanyan48@gmail.com>
2025-10-21 00:13:10 +0400

    Delete db.js

--- db.js
+++
@@ -1,5 +0,0 @@
-const sqlite3=require('sqlite3').verbose();const db=new sqlite3.Database('./rb.sqlite');
-db.serialize(()=>{db.run(`CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT,telegram_id TEXT,username TEXT,balance INTEGER DEFAULT 0,total_deposited INTEGER DEFAULT 0,created_at DATETIME DEFAULT CURRENT_TIMESTAMP)`);
-db.run(`CREATE TABLE IF NOT EXISTS txs(id INTEGER PRIMARY KEY AUTOINCREMENT,txid TEXT UNIQUE,amount INTEGER,status TEXT,created_at DATETIME DEFAULT CURRENT_TIMESTAMP)`);
-db.run(`CREATE TABLE IF NOT EXISTS withdraws(id INTEGER PRIMARY KEY AUTOINCREMENT,user_id INTEGER,amount INTEGER,address TEXT,status TEXT DEFAULT 'pending',created_at DATETIME DEFAULT CURRENT_TIMESTAMP)`);});
-module.exports=db;
